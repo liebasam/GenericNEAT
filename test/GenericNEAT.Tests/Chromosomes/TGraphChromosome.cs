c6d70d4d1ad12e884cbaa9b08fae78119fc6a5fd
liebasam <l.samuel.cook@gmail.com>
2020-02-22 15:08:56 -0500

    Added test stubs.

---
+++ test/GenericNEAT.Tests/Chromosomes/TGraphChromosome.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericNEAT.Chromosomes.Tests
+{
+    [TestClass]
+    public class TGraphChromosome
+    {
+        [TestClass]
+        public class Constructors
+        {
+            [TestMethod]
+            public void Failure()
+            {
+                Assert.Fail();
+            }
+
+            [TestMethod]
+            public void TwoArgs()
+            {
+                Assert.Fail();
+            }
+
+            [TestMethod]
+            public void ThreeArgs()
+            {
+                Assert.Fail();
+            }
+
+            [TestMethod]
+            public void FourArgs()
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void Clone()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CreateNew()
+        {
+            Assert.Fail();
+        }
+    }
+}
