12c833faad3330252de270632381cbc7ad6bc6da
liebasam <l.samuel.cook@gmail.com>
2020-02-29 20:02:27 -0500

    Added SpeciedPopulation

--- src/GenericNEAT/Populations/Specie.cs
+++ src/GenericNEAT/Populations/Specie.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections;
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Populations;
 using System.Collections.Generic;
 
 namespace GenericNEAT.Populations
@@ -7,64 +7,33 @@ namespace GenericNEAT.Populations
     /// <summary>
     /// Collection of items with a centroid and unique ID.
     /// </summary>
-    public sealed class Specie<T> : ICollection<T>, ICloneable
+    public class Specie : Population
     {
-        readonly List<T> _items;
-
         /// <summary>
-        /// Center of the specie. Is not necessarily a member
-        /// of the collection.
-        /// </summary>
-        public T Centroid { get; }
-
-        /// <summary>
-        /// Unique identifier.
+        /// The ID of the specie.
         /// </summary>
         public uint ID { get; }
 
-        public int Count => _items.Count;
-
-        public bool IsReadOnly => false;
-
-        #region Constructors
         /// <summary>
-        /// Creates an empty specie.
+        /// The defining member of the specie.
         /// </summary>
-        public Specie(uint id, T centroid)
+        public IChromosome Centroid { get; }
+
+        /// <summary>
+        /// Creates a new specie.
+        /// </summary>
+        public Specie(int minSize, int maxSize, uint id, IChromosome centroid) : base(minSize, maxSize, centroid)
         {
             ID = id;
             Centroid = centroid;
         }
 
-        /// <summary>
-        /// Creates a pre-populated specie.
-        /// </summary>
-        public Specie(uint id, T centroid, IEnumerable<T> collection)
+        public override void CreateInitialGeneration()
         {
-            ID = id;
-            Centroid = centroid;
-            _items = new List<T>(collection);
+            var tmp = MinSize;
+            MinSize = MaxSize;
+            base.CreateInitialGeneration();
+            MinSize = tmp;
         }
-        #endregion
-
-        #region Collection Methods
-        public void Add(T item) => _items.Add(item);
-
-        public void Clear() => _items.Clear();
-
-        public bool Contains(T item) => _items.Contains(item);
-
-        public void CopyTo(T[] array, int arrayIndex) => _items.CopyTo(array, arrayIndex);
-
-        public bool Remove(T item) => _items.Remove(item);
-
-        public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => _items.GetEnumerator();
-        #endregion
-
-        #region Methods
-        public object Clone() => new Specie<T>(ID, Centroid, _items);
-        #endregion
     }
 }
--- src/GenericNEAT/Populations/SpeciedPopulation.cs
+++ src/GenericNEAT/Populations/SpeciedPopulation.cs
@@ -1,43 +1,115 @@
 ﻿using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Populations;
+using GeneticSharp.Domain.Randomizations;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace GenericNEAT.Populations
 {
-    public class SpeciedPopulation : IPopulation
+    public class SpeciedPopulation : Population
     {
-        public DateTime CreationDate { get; } = DateTime.Now;
+        readonly List<Specie> m_species;
 
-        public IList<Generation> Generations => throw new NotImplementedException();
+        /// <summary>
+        /// Enumerates the species in the population.
+        /// </summary>
+        public ICollection<Specie> Species => m_species;
 
-        public Generation CurrentGeneration => throw new NotImplementedException();
+        /// <summary>
+        /// Number of species in the population.
+        /// </summary>
+        public int SpecieCount => m_species.Count;
 
-        public int GenerationsNumber => throw new NotImplementedException();
+        /// <summary>
+        /// Minimum size of any specie.
+        /// </summary>
+        public int MinSpecieSize { get; set; }
 
-        public int MinSize { get; set; }
-        public int MaxSize { get; set; }
+        public ISpeciationStrategy SpeciationStrategy { get; set; }
 
-        public IChromosome BestChromosome => throw new NotImplementedException();
+        /// <summary>
+        /// ID that will be used when creating a new specie.
+        /// </summary>
+        protected uint NextSpecieID { get; set; }
 
-        public IGenerationStrategy GenerationStrategy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public event EventHandler BestChromosomeChanged;
-
-        public void CreateInitialGeneration()
+        /// <summary>
+        /// Creates a new specied population.
+        /// </summary>
+        public SpeciedPopulation(int minSize, int maxSize, IChromosome adamChromosome,
+            ISpeciationStrategy speciationStrategy, int minSpecieSize) : base(minSize, maxSize, adamChromosome)
         {
-            throw new NotImplementedException();
+            if (speciationStrategy is null)
+                throw new ArgumentNullException(nameof(speciationStrategy));
+            if (minSpecieSize < 0)
+                throw new ArgumentOutOfRangeException(nameof(minSpecieSize));
+            SpeciationStrategy = speciationStrategy;
+            MinSpecieSize = minSpecieSize;
         }
 
-        public void CreateNewGeneration(IList<IChromosome> chromosomes)
+        #region Methods
+        public override void CreateInitialGeneration()
         {
-            throw new NotImplementedException();
+            // Reset the species list
+            m_species.Clear();
+            var specie = new Specie(MinSpecieSize, MinSize, 0u, AdamChromosome);
+            specie.CreateInitialGeneration();
+            m_species.Add(specie);
+            NextSpecieID = 1u;
+
+            // Reset the base generations list
+            Generations.Clear();
+            base.CreateNewGeneration(specie.CurrentGeneration.Chromosomes);
         }
 
-        public void EndCurrentGeneration()
+        public override void CreateNewGeneration(IList<IChromosome> chromosomes)
         {
-            throw new NotImplementedException();
+            base.CreateNewGeneration(chromosomes);
+
+            // Organize chromosomes by species
+            var lookup = chromosomes.ToLookup(c => GetSpecie(c));
+            foreach (var group in lookup)
+            {
+                // Add em to the correct specie
+                group.Key.CreateNewGeneration(group.ToList());
+            }
+
+            // Remove empty species
+            for (int i = 0; i < SpecieCount; i++)
+            {
+                if (m_species[i].CurrentGeneration.Chromosomes.Count == 0)
+                {
+                    m_species.RemoveAt(i);
+                    i--;
+                }
+            }
         }
+
+        public override void EndCurrentGeneration()
+        {
+            base.EndCurrentGeneration();
+            for (int i = 0; i < SpecieCount; i++)
+                m_species[i].EndCurrentGeneration();
+        }
+
+        /// <summary>
+        /// Returns the first specie to which <paramref name="chromosome"/>
+        /// fits, according too <see cref="SpeciationStrategy"/>.
+        /// </summary>
+        protected Specie GetSpecie(IChromosome chromosome)
+        {
+            for (int i = 0; i < SpecieCount; i++)
+            {
+                var curSpecie = m_species[i];
+                if (SpeciationStrategy.AreSameSpecies(chromosome, curSpecie.Centroid))
+                    return curSpecie;
+            }
+
+            // Create a new specie
+            var specie = new Specie(MinSpecieSize, MaxSize, ++NextSpecieID, chromosome);
+            m_species.Add(specie);
+            return specie;
+        }
+        #endregion
     }
 }
