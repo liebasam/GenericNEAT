45ddad8bd671fec8ac0e3963337eb858c7615bb9
liebasam <l.samuel.cook@gmail.com>
2020-03-01 11:59:39 -0500

    Add tests for graph mutations

---
+++ test/GenericNEAT.Tests/Operators/TAddEdgeMutation.cs
@@ -0,0 +1,31 @@
+﻿using GenericNEAT.Chromosomes;
+using GeneticSharp.Domain.Chromosomes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericNEAT.Operators.Tests
+{
+    [TestClass]
+    public class TAddEdgeMutation
+    {
+        GraphChromosome Graph;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            Graph = new GraphChromosome(TestHelpers.IntegerChromosome(), TestHelpers.IntegerChromosome());
+            Graph.AddVertex(0u);
+            Graph.AddVertex(1u);
+            Graph.AddEdge(0u, 1u);
+        }
+
+        [TestMethod]
+        public void Mutate()
+        {
+            var mut = new AddEdgeMutation();
+            mut.Mutate(Graph, 0);
+            Assert.AreEqual(1, Graph.EdgeCount);
+            mut.Mutate(Graph, 1);
+            Assert.AreEqual(2, Graph.EdgeCount);
+        }
+    }
+}
---
+++ test/GenericNEAT.Tests/Operators/TSplitEdgeMutation.cs
@@ -0,0 +1,37 @@
+﻿using GenericNEAT.Chromosomes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericNEAT.Operators.Tests
+{
+    [TestClass]
+    public class TSplitEdgeMutation
+    {
+        GraphChromosome Graph;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            Graph = new GraphChromosome(TestHelpers.IntegerChromosome(), TestHelpers.IntegerChromosome());
+            Graph.AddVertex(0u);
+            Graph.AddVertex(1u);
+            Graph.AddEdge(0u, 1u);
+        }
+        
+        [TestMethod]
+        public void Mutate()
+        {
+            var factory = new IDFactory(3u);
+            var mut = new SplitEdgeMutation(factory);
+            mut.Mutate(Graph, 0);
+            Assert.AreEqual(2, Graph.VertexCount);
+            Assert.AreEqual(1, Graph.EdgeCount);
+            mut.Mutate(Graph, 1);
+            Assert.AreEqual(3, Graph.VertexCount);
+            Assert.IsTrue(Graph.ContainsVertex(3u));
+            Assert.AreEqual(2, Graph.EdgeCount);
+            Assert.IsTrue(Graph.ContainsEdge(0u, 3u));
+            Assert.IsTrue(Graph.ContainsEdge(3u, 1u));
+            Assert.IsFalse(Graph.ContainsEdge(0u, 1u));
+        }
+    }
+}
